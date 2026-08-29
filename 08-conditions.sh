3bb268785228e23480cea367bbff82230475792f
deepak7989 <deepakvadla05@gmail.com>
2026-08-29 19:32:28 +0530

    hello world

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -9,8 +9,10 @@ NUMBER=$1
 # ge - greater than or equal
 # le - less than or equal
 
-if [ $NUMBER -ge 20 ]; then
+if [ $NUMBER -gt 20 ]; then
     echo "Given number $NUMBER is greater than or equal to 20"
+else if[ $NUMBER -eq 20 ]
+    echo "number is equal to 20"
 else
     echo "number is less than 20"
 fi
