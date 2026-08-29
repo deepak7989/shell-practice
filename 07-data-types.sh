d1bad67ab9ed23f81cab0718debc13eafc59bcc5
deepak7989 <deepakvadla05@gmail.com>
2026-08-29 19:06:38 +0530

    hello world

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -8,5 +8,5 @@ SUM=$(($NUM1+$NUM2))
 echo "Sum is: $SUM"
 
 #Aray
-MOVIES=("RRR", "VARANASI", "PUSHPA")#Index always starts from 0
+MOVIES=("RRR" "VARANASI" "PUSHPA")#Index always starts from 0
 echo "Movies are: ${MOVIES[@]}"
