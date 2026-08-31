e1b4b655e953e3cd948e8cfe2c98d2252c8736ff
deepak7989 <deepakvadla05@gmail.com>
2026-08-31 13:43:27 +0530

    hello world

---
+++ 12-loops.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#!/bin/bash
+
+USERID=$(id -u)
+
+# check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+#first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... Failed"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+
+}
+
+for package in $@
+do
+    echo "Installing $pacakge"
+done
+
