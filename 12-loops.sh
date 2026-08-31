3bb33a893d4ec874eb6b14ab1356ee1510641534
deepak7989 <deepakvadla05@gmail.com>
2026-08-31 14:09:10 +0530

    hello world

--- 12-loops.sh
+++ 12-loops.sh
@@ -5,6 +5,7 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 # check root access or not
 if [ $USERID -ne 0 ]; then
@@ -16,10 +17,10 @@ fi
 # second arg -> exit code
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "Installing $1 is ... Failed"
+        echo "$TIMESTAMP [ERROR] Installing $1 is ... Failed" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "Installing $1 is ... SUCCESS"
+        echo "$TIMESTAMP [INFO] Installing $1 is ... SUCCESS" | tee -a $LOGS_FILE
     fi
 
 }
@@ -27,7 +28,7 @@ VALIDATE(){
 for package in $@
 do
     echo "Installing $package"
-    dnf list installed $package
+    dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE "Installing $package" $?
