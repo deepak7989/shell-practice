492c14b6ba71785c41401b30e9eace54b3524b4b
deepak7989 <deepakvadla05@gmail.com>
2026-08-31 14:13:38 +0530

    hello world

--- 12-loops.sh
+++ 12-loops.sh
@@ -27,7 +27,7 @@ VALIDATE(){
 
 for package in $@
 do
-    echo "Installing $package"
+    echo "$TIMESTAMP [INFO] Installing $package"
     dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         dnf install $package -y &>> $LOGS_FILE
