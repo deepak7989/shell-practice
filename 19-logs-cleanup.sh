641b64ae7604feadace8ff7060b8304198cf93e4
deepak7989 <deepakvadla05@gmail.com>
2026-09-04 17:41:58 +0530

    api doc added

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -5,7 +5,7 @@ DAYS=${2:-14} # default 14days
 
 if [ -z $SCRIPT_DIR ]; then
     echo "ERROR:: Missing parameters"
-    echo "USAGE: $0 <source-dir> [days(optional default to 14)]"
+    echo "USAGE:: $0 <source-dir> [days(optional default to 14)]"
     exit 1
 fi
 
